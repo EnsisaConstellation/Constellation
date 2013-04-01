ea141b9e32780d857e3fd01549f1826b20f48722
Robin <w.land@laposte.net>
2013-04-01 15:21:59 +0200

    suppression d'un dossier
    la première version du jeu n'a plus d'interet

--- chatRobin_v0.0.1/src/constellation/chat/robin/Serveur.java
+++ chatRobin_v0.0.1/src/constellation/chat/robin/Serveur.java
@@ -12,17 +12,6 @@ public class Serveur {
     public static void main(String[] args) {
         Map<Integer, String> messages = Hazelcast.getMap("messages");
         Map<Integer, String> auteurs = Hazelcast.getMap("auteurs");
-        
-        auteurs.put(auteurs.size(), "un con");
-        messages.put(messages.size(), "cet idiot est completement con");
-        
-        auteurs.put(auteurs.size(), "un idiot");
-        messages.put(messages.size(), "ce con est un idiot");
-
-        //System.out.println("Customer with key 1: "+ mapCustomers.get(1));
-        //System.out.println("Map Size:" + mapCustomers.size());
-
-
     }
 	
 	
