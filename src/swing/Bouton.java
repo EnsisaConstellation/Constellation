3c4c4cda3f43961c01dcd505ee39d06ba333da5a
Gaechter Tristan <secretsecrets@laposte.net>
2013-04-21 00:42:58 +0200

    MAJ Client + simplification du bouton swing
    MAJ Client :
    - ajout de fonctions suite à l'ajout des Commandes
    - modification de la méthode deconnexion()
    
    MAJ Bouton swing :
    - simplification du code au strict minimum en vu de corriger le pb
    visuel (causé par l'absence d'image)

--- src/swing/Bouton.java
+++ src/swing/Bouton.java
@@ -17,97 +17,42 @@ import javax.swing.JButton;
 public class Bouton extends JButton implements MouseListener {
 
 	 private String name;
-	 private Image img;
 	 
 	 public Bouton(String str){
 	    super(str);
 	    this.name = str;
-	    // inser une image de fond pour le bouton 
-	    /*try {
-	      img = ImageIO.read(new File("orange.png"));
-	        } 
-	    catch (IOException e) 
-	    {
-	      e.printStackTrace();
-	    }*/
 	    //Grâce à cette instruction, notre objet va s'écouter
 	    //Dès qu'un événement de la souris sera intercepté, il en sera averti
 	    this.addMouseListener(this);
 	  }
-	 // methode pour inserer les forme graphique 
-	  public void paintComponent(Graphics g){
-	    Graphics2D g2d = (Graphics2D)g;
-	   
-	    g2d.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-	    g2d.setColor(Color.black);
-	    g2d.drawString(this.name, this.getWidth() /2 - (this.getWidth() /  2 /4), (this.getHeight() / 2) );
-	  }
-	
-	
-	
-	
-	
 	
 	  //Méthode appelée lors du clic de souris
-	  public void mouseClicked(MouseEvent event)
-	  {
-		 /* try 
-		  {
-		      img = ImageIO.read(new File("gris.png"));
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }*/
+	  public void mouseClicked(MouseEvent event){
+		
 	  }
 	 
 	  //Méthode appelée lors du survol de la souris
-	  public void mouseEntered(MouseEvent event)
-	  {
-		 /* try {
-		      img = ImageIO.read(new File("jaune.png"));
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }*/
+	  public void mouseEntered(MouseEvent event){
 		  
 	  }
 	 
 	  //Méthode appelée lorsque la souris sort de la zone du bouton
-	  public void mouseExited(MouseEvent event) 
-	  {
-		 /* try {
-		      img = ImageIO.read(new File("orange.png"));
-		        } 
-		    catch (IOException e) 
-		    {
-		      e.printStackTrace();
-		    }*/
+	  public void mouseExited(MouseEvent event){
 		  
 	  }
 	 
 	  //Méthode appelée lorsque l'on presse le bouton gauche de la souris
-	  public void mousePressed(MouseEvent event) 
-	  {/*try
-	  {
-	      img = ImageIO.read(new File("gris.png"));
-      } 
-      catch (IOException e) 
-      {
-          e.printStackTrace();
-       }*/
-       }
+	  public void mousePressed(MouseEvent event){
+		  
+      }
 	 
 	  //Méthode appelée lorsque l'on relâche le clic de souris
-	  public void mouseReleased(MouseEvent event)
-	  { 
-		 /* try {
-		      img = ImageIO.read(new File("orange.png"));
-		        } 
-		    catch (IOException e) 
-		    {
-		      e.printStackTrace();
-		    }*/
+	  public void mouseReleased(MouseEvent event){ 
 		  
-	  }       
+	  }   
+	  
 	}
+
 	
 	
 	
